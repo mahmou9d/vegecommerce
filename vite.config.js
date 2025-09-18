87662132e7d1e27887ebd4cf3c6c2dc7f80f86df
mahm9oud <eatshouse@gmail.com>
2025-09-18 12:31:35 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://e-commerce-web-production-ead4.up.railway.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://e-commerce-web-production-ead4.up.railway.app",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, "/api"),
+  //     },
+  //   },
+  // },
 });
